8119561de99882f8c35ec3eca221a5753412c2e1
sal <sal@salpadding.local>
2021-07-27 11:27:29 +0800

    finish ins pool

--- rust/src/types/ins_pool.rs
+++ rust/src/types/ins_pool.rs
@@ -1,7 +1,7 @@
 use std::fs::OpenOptions;
 use std::io::{Cursor, Seek, SeekFrom};
 
-use parity_wasm::elements::{BlockType, Instruction, opcodes, ValueType, VarUint32, Deserialize, VarInt32, VarUint64, VarInt64};
+use parity_wasm::elements::{BlockType, Instruction, opcodes, ValueType, VarUint32, Deserialize, VarInt32, VarUint64, VarInt64, Uint32, Uint64};
 
 use crate::StringErr;
 use std::collections::BTreeSet;
@@ -30,9 +30,11 @@ trait ModuleCursor<T> {
     trait_var!(i32, var_i32, VarInt32);
     trait_var!(u64, var_u64, VarUint64);
     trait_var!(i64, var_i64, VarInt64);
+    trait_var!(u32, u32, Uint32);
+    trait_var!(u64, u64, Uint64);
 }
 
-pub const NULL: u64 = 0xFFFFFFFFFFFFFFFFu64;
+pub const NULL: u64 = 0;
 
 
 
@@ -57,6 +59,8 @@ impl <T: AsRef<[u8]>> ModuleCursor<u8> for Cursor<T>  {
     impl_var!(i32, var_i32, VarInt32);
     impl_var!(u64, var_u64, VarUint64);
     impl_var!(i64, var_i64, VarInt64);
+    impl_var!(u32, u32, Uint32);
+    impl_var!(u64, u64, Uint64);
 }
 
 // allocation for runtime instruction
@@ -72,6 +76,28 @@ pub(crate) struct InsBits(u64);
 
 #[derive(Copy, Clone)]
 pub(crate) struct InsVec(u64);
+
+impl InsVec {
+    pub(crate) fn is_null(&self) -> bool {
+        self.0 == NULL
+    }
+
+    pub(crate) fn size(&self) -> usize {
+        ((self.0 >> 32) & 0xffffffffu64) as usize
+    }
+
+    pub(crate) fn offset(&self) -> usize {
+        (self.0 & 0xffffffffu64) as usize
+    }
+
+    pub(crate) fn null() -> InsVec {
+        InsVec(NULL)
+    }
+
+    pub(crate) fn new(offset: u32, size: u32) -> InsVec {
+        InsVec(((size as u64) << 32) | (offset as u64))
+    }
+}
 
 impl InsBits {
     pub(crate) fn new(op: u8) -> Self {
@@ -119,7 +145,7 @@ impl InsBits {
             1 => Some(ValueType::I64),
             2 => Some(ValueType::F32),
             3 => Some(ValueType::F64),
-            _ => panic!("unreachable")
+            _ => panic!("unexpected block type")
         }
     }
 
@@ -152,10 +178,16 @@ pub struct InsPool {
 }
 
 impl InsPool {
-    pub(crate) fn push(&mut self, ops: &[Instruction]) -> InsVec {
-        InsVec(0)
+    fn new() -> Self {
+        // push a null pointer
+        let mut r = InsPool { data: Vec::new() };
+        r.data.push(NULL);
+        r
     }
+}
 
+
+impl InsPool {
     // store an instruction
     fn alloc_ins(&mut self, ins: InsBits) -> usize {
         let r = self.data.len();
@@ -183,7 +215,7 @@ impl InsPool {
         r
     }
 
-    fn span_linked(&mut self, head: u32) -> InsVec {
+    fn span_linked(&mut self, head: u32) -> Result<InsVec, StringErr> {
         let mut cnt: u32 = 0;
         let mut cur = self.data[head as usize];
         let start = self.data.len();
@@ -200,16 +232,71 @@ impl InsPool {
 
             cur = self.data[next as usize];
         }
-        InsVec(((cnt as u64) << 32) | (start as u64))
+
+        if cnt > u16::MAX as u32{
+            Err(StringErr::new("expression size overflow"))
+        } else {
+            Ok(InsVec::new(start as u32, cnt))
+        }
+    }
+    
+    fn operand(&self, ins: InsBits, i: usize) -> u64 {
+        let off = ins.payload() as usize;
+        self.data[off + i]
     }
 
-    fn read_util<'b>(&mut self, cursor: &'b mut Cursor<Vec<u8>>, end: &[u8]) -> Result<InsVec, StringErr> {
-        let mut s: BTreeSet<u8> = BTreeSet::new();
-        for x in end {
-            s.insert(*x);
-        }
+    pub(crate) fn read_expr(&mut self, cursor: &mut Cursor<Vec<u8>>) -> Result<InsVec, StringErr> {
+        let r = self.read_util(cursor, &[opcodes::END])?;
+        cursor.next()?;
+        Ok(r)
+    }
 
-        Err(StringErr::new("TODO"))
+    fn read_util(&mut self, cursor: &mut Cursor<Vec<u8>>, end: &'static [u8]) -> Result<InsVec, StringErr> {
+        let mut cnt = -1;
+        // head is a pointer
+        let mut head: u32 = 0;
+        let mut cur = 0;
+
+        loop {
+            cnt += 1;
+            let ins = cursor.peek()?;
+            if end.iter().any(|x| *x == ins) {
+                if head == 0 {
+                    return Ok(InsVec::null())
+                }
+                return self.span_linked(head);
+            }
+
+            let read = self.read_ins(cursor)?;
+            match head {
+                0 => {
+                    let ptr = self.alloc_ins(read) as u32;
+                    head = self.alloc_linked(ptr) as u32;
+                    if head == 0 {
+                        println!("alloc lined = 0")
+                    }
+                    cur = head;
+                }
+                _ => {
+                    let ptr = self.alloc_ins(read) as u32;
+                    cur = self.push_linked(cur, ptr) as u32;
+                }
+            }
+        }
+    }
+
+    fn read_ins(&mut self, cursor: &mut Cursor<Vec<u8>>) -> Result<InsBits, StringErr> {
+        let op = cursor.peek()?;
+
+        match op {
+            // control instructions
+            opcodes::UNREACHABLE..=opcodes::CALLINDIRECT => self.read_ctl(cursor),
+            // parametric
+            opcodes::DROP..=opcodes::SELECT => Ok(InsBits::no_result(cursor.next()?)),
+            opcodes::GETLOCAL..=opcodes::SETGLOBAL => self.read_var_ins(cursor),
+            opcodes::I32LOAD..=opcodes::GROWMEMORY => self.read_mem_ins(cursor),
+            _ => self.read_num_ins(cursor)
+        }
     }
 
     fn push_labels(&mut self, cursor: &mut Cursor<Vec<u8>>) -> Result<u32, StringErr> {
@@ -224,7 +311,7 @@ impl InsPool {
 
 
 
-    fn push_ctl(&mut self, cursor: &mut Cursor<Vec<u8>>) -> Result<InsBits, StringErr> {
+    fn read_ctl(&mut self, cursor: &mut Cursor<Vec<u8>>) -> Result<InsBits, StringErr> {
         let op: u8 = cursor.next()?;
         let mut bits = InsBits::no_result(op);
         return match op {
@@ -241,7 +328,7 @@ impl InsPool {
                 let t: BlockType = BlockType::deserialize(cursor)?;
                 bits = bits.add_block_type(t);
                 let branch_0 = self.read_util(cursor, &[opcodes::END, opcodes::ELSE])?;
-                let mut branch_1 = InsVec(NULL);
+                let mut branch_1 = InsVec::null();
 
                 if cursor.peek()? == opcodes::ELSE {
                     // skip 0x05
@@ -273,18 +360,18 @@ impl InsPool {
                 }
             }
             _ => {
-                Err(StringErr::new("unreachable"))
+                Err(StringErr::new(format!("unexpected op {}", op)))
             }
         }
     }
 
-    fn get_var_ins(&self, cur: &mut Cursor<Vec<u8>>) -> Result<InsBits, StringErr> {
+    fn read_var_ins(&self, cur: &mut Cursor<Vec<u8>>) -> Result<InsBits, StringErr> {
         let op = cur.next()?;
         let bits = InsBits::no_result(op).add_payload(cur.var_u32()?);
         Ok(bits)
     }
 
-    fn get_mem_ins(&self, cur: &mut Cursor<Vec<u8>>) -> Result<InsBits, StringErr> {
+    fn read_mem_ins(&self, cur: &mut Cursor<Vec<u8>>) -> Result<InsBits, StringErr> {
         let op = cur.next()?;
         let bits = InsBits::no_result(op);
 
@@ -301,20 +388,75 @@ impl InsPool {
         )
     }
 
-    // fn push_num_ins(&mut self, cur: &mut Cursor<Vec<u8>>) -> Result<InsBits, StringErr> {
-    //     let op = cur.next()?;
-    //     let bits = InsBits::no_result(op);
-    //     let bits =
-    //     match op {
-    //         opcodes::I32CONST => bits.add_payload(cur.var_u32()?).add_operand_size(1),
-    //         opcodes::I64CONST => {
-    //             bits = self.add_body_off(bits);
-    //
-    //             bits
-    //         }
-    //         _ => {
-    //             return Err(StringErr::new("unreachable"))
-    //         }
-    //     }
-    // }
+    fn ins_in_vec(&self, vec: InsVec, i: usize) -> InsBits {
+        InsBits(self.data[vec.offset() + i])
+    }
+
+    fn branch0(&self, ins: InsBits) -> InsVec {
+        let off = ins.payload();
+        InsVec(self.data[off])
+    }
+
+    fn branch1(&self, ins: InsBits) -> InsVec {
+        let off = ins.payload();
+        InsVec(self.data[off + 1])
+    }
+
+    fn read_num_ins(&mut self, cur: &mut Cursor<Vec<u8>>) -> Result<InsBits, StringErr> {
+        let op = cur.next()?;
+        let bits = InsBits::no_result(op);
+        let bits =
+        match op {
+            opcodes::I32CONST => bits.add_payload(cur.var_i32()? as u32).add_operand_size(1),
+            opcodes::I64CONST => {
+                let r = self.add_body_off(bits).add_operand_size(1);
+                self.data.push(cur.var_i64()? as u64);
+                r
+            }
+            opcodes::F32CONST => {
+                bits.add_payload(cur.u32()?).add_operand_size(1)
+            }
+            opcodes::F64CONST => {
+                let r = self.add_body_off(bits).add_operand_size(1);
+                self.data.push(cur.u64()?);
+                r
+            }
+            _ => bits
+        };
+        Ok(bits)
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use crate::types::ins_pool::InsPool;
+    use std::fs::File;
+    use std::fs;
+    use std::io::{Read, Cursor};
+    use parity_wasm::elements::{Deserialize, Serialize};
+    use crate::StringErr;
+
+
+    fn test() -> Result<(), StringErr>{
+        let mut pool = InsPool::new();
+        let filename = "src/testdata/main.wasm";
+        let mut f = File::open(filename).expect("no file found");
+        let m = parity_wasm::elements::Module::deserialize(&mut f)?;
+
+        let code_sec = m.code_section().unwrap();
+
+        for bd in code_sec.bodies() {
+            let mut serialized: Vec<u8> = Vec::new();
+            bd.code().to_owned().serialize(&mut serialized)?;
+            let mut cur = Cursor::new(serialized);
+            pool.read_expr(&mut cur)?;
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_1() {
+        test().unwrap()
+    }
 }
